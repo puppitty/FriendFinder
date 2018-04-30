6421f007fc3e050cbdcff6b9da0c603c2b473233
puppitty <33668867+puppitty@users.noreply.github.com>
2018-04-30 18:10:10 -0400

    updating

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -15,14 +15,14 @@ module.exports = function (app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
   app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "/public/home.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   })
   app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/survey.html"));
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
   // If no matching route is found default to home
   app.get("/home", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/home.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
